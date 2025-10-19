9d6978aa5f9818a18b5a90019cd4b4e5188ba787
soyeun <thduswkd96@gmail.com>
2025-10-19 22:03:45 +0900

    feat(main) : 예외 처리

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -3,6 +3,7 @@ package calculator;
 import camp.nextstep.edu.missionutils.Console;
 
 import java.util.Arrays;
+import java.util.regex.Pattern;
 
 public class Application {
     public static void main(String[] args) {
@@ -17,11 +18,12 @@ public class Application {
         } else {
             String[] inputSplit;
             if (input.contains("//") && input.contains("\\n")) {
+                isValidCustomDelimiter(input);
                 inputSplit = splitByCustomDelimiter(input);
             } else {
                 inputSplit = splitByDefaultDelimiter(input);
             }
-            
+
             result = sumNum(inputSplit);
             output(result);
         }
@@ -44,15 +46,52 @@ public class Application {
         int end = input.indexOf("\\n");
 
         String between = input.substring(start + 2, end);
+        isValidBetween(between);
+
         String numbers = input.substring(end + 2);
 
-        return numbers.split(between);
+        return numbers.split(Pattern.quote(between));
+    }
+
+    public static void isValidCustomDelimiter(String input) {
+        int start = input.indexOf("//");
+        int end = input.indexOf("\\n");
+
+        if (start == -1 || end == -1 || end <= start + 2) {
+            throw new IllegalArgumentException("커스텀 구분자가 잘못되었습니다.");
+        }
+
+        if (start > 0) {
+            throw new IllegalArgumentException("커스텀 구분자가 처음부터 시작하지 않습니다.");
+        }
+    }
+
+    public static void isValidBetween(String between) {
+        if (between.isEmpty()) {
+            throw new IllegalArgumentException("커스텀 구분자가 존재하지 않습니다.");
+        }
+
+        if (between.length() > 1) {
+            throw new IllegalArgumentException("커스텀 구분자가 한 자 이상입니다.");
+        }
+
+        if (between.matches("\\d+")) {
+            throw new IllegalArgumentException("커스텀 구분자가 숫자입니다.");
+        }
     }
 
     public static int sumNum(String[] inputSplit) {
-        int numberSum = Arrays.stream(inputSplit)
-                .mapToInt(Integer::parseInt)
-                .sum();
-        return numberSum;
+        try {
+            return Arrays.stream(inputSplit)
+                    .mapToInt(Integer::parseInt)
+                    .peek(n -> {
+                        if (n < 0) {
+                            throw new IllegalArgumentException("음수는 허용되지 않습니다.");
+                        }
+                    })
+                    .sum();
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("허용되지 않은 구분자가 포함되어 있습니다.");
+        }
     }
 }
