b8df35f280baa3d0fa1bdd9278bc08558ddabfaf
soyeun <thduswkd96@gmail.com>
2025-10-19 11:49:56 +0900

    feat(main) : 문자열 입력 & 출력

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,25 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = Console.readLine();
+
+        int result = 0;
+        if (!isNull(input)) {
+            result = Integer.parseInt(input);
+        }
+        output(result);
+    }
+
+    public static void output(int result) {
+        System.out.println("결과 : " + result);
+    }
+
+    public static boolean isNull(String input) {
+        return input == null || input.isEmpty();
     }
 }
